3c6579689d3bfa4f3636b920212106df5dcc4354
Noyha <noyhadaria@gmail.com>
2020-02-07 15:32:16 +0200

    Updates

---
+++ precache-manifest.f8aaea7d9b9108bb62fb4d4ff791ed82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7fbdda31cf93e93bd6fcea5399c56b",
+    "url": "/monsters-react-app/index.html"
+  },
+  {
+    "revision": "fed55995d1a6b2083911",
+    "url": "/monsters-react-app/static/css/main.01105a73.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters-react-app/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-react-app/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed55995d1a6b2083911",
+    "url": "/monsters-react-app/static/js/main.c167ec3c.chunk.js"
+  },
+  {
+    "revision": "c9c2abec6b877d0bc428",
+    "url": "/monsters-react-app/static/js/runtime-main.2d21b0b4.js"
+  }
+]);
